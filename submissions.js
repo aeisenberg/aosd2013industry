8183a894ca433e4f1fac546f1ad44dc0e2637041
root <root@w2s2-orion-dev.eng.vmware.com>
2012-03-22 11:16:59 -0700

    initial commit

---
+++ submissions.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2007 Andrew Eisenberg
+// Submission system for AOSD 2013 Industry track
+// Redistributable under a BSD-style open source license.
+// See http://www.linfo.org/bsdlicense.html
+
+// Submissions namespace
+var submissions = {
+	init : function() {
+	
+	}
+};
+
+submissions.init();
